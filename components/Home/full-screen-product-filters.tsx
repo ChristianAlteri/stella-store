9fb63b6c461b8372b0086b46d97459c71d514b2d
ChristianAlteri <alteri.christian@gmail.com>
2024-04-24 19:37:20 +0100

    starting sale page

--- components/Home/full-screen-product-filters.tsx
+++ components/Home/full-screen-product-filters.tsx
@@ -24,7 +24,7 @@ const FullscreenProductFilters: React.FC<FullscreenProductFiltersProps> = ({
   return (
     <div className="flex-col justify-center items-center p-2 shadow-2xl rounded-md w-1/4  h-full border hidden md:flex">
       <div className="flex flex-col items-center p-4 shadow-lg rounded-md w-full h-full">
-        {categoryData?.name && <h1>{categoryData.name}</h1>}
+        {/* {categoryData?.name && <h1>{categoryData.name}</h1>} */}
         <div className="flex flex-row justify-center items-center w-full">
           {genders?.[0] && (
             <div className="h-full w-full">
