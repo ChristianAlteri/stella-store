004dcb6a2bc60aa79bd4df53ac2d878369bbe4be
ChristianAlteri <alteri.christian@gmail.com>
2024-04-28 11:51:38 +0100

    working on search - added show all feature - added seller cat and desginer

--- components/Search/mega-search.tsx
+++ components/Search/mega-search.tsx
@@ -22,11 +22,11 @@ import {
 import IconButton from "../ui/icon-button";
 import ShoppingCartButton from "../NavBar/ShoppingCartButton";
 
-
 import SearchQuickLinks from "./search-quick-links";
-import SearchInputAndResultsSellers from "./search-input-and-results-sellers";
+import SearchInputAndResultsData from "./search-input-and-results-data";
 import SearchInputAndResultsProducts from "./search-input-and-results-products";
 import { usePathname } from "next/navigation";
+import { cn } from "@/lib/utils";
 
 interface MegaSearchProps {
   products: Product[] | undefined;
@@ -38,7 +38,7 @@ interface MegaSearchProps {
   conditions?: Condition[];
   materials?: Material[];
   genders?: Gender[];
-  subcategories?: Subcategory[]; 
+  subcategories?: Subcategory[];
   icon?: React.ReactNode;
 }
 
@@ -56,12 +56,11 @@ const MegaSearch: React.FC<MegaSearchProps> = ({
   icon,
 }) => {
   const [open, setOpen] = useState(false);
-  const [searchBy, setSearchBy] = useState('Product');
+  const [searchBy, setSearchBy] = useState("Product");
   const pathname = usePathname();
   useEffect(() => {
     setOpen(false);
   }, [pathname]);
-
 
   const onOpen = () => setOpen(true);
   const onClose = () => setOpen(false);
@@ -85,24 +84,21 @@ const MegaSearch: React.FC<MegaSearchProps> = ({
         <div className="fixed justify-center items-center inset-0 z-40 flex">
           <Dialog.Panel className="flex rounded-sm flex-col h-2/3 w-2/3 overflow-y-auto bg-white shadow-xl border border-light-font">
             <div className="flex flex-col w-full h-full items-center justify-center ">
-
-              <div className="flex flex-row justify-between w-full p-2 top-0 items-center text-center ">
+              {/* <div className="flex flex-row justify-between w-full p-2 top-0 items-center text-center ">
                 <div className="">
                   <IconButton
                     icon={<IoCloseOutline size={17} />}
                     onClick={onClose}
                   />
                 </div>
-                {/* Cart */}
-                <div className="">
-                  <ShoppingCartButton
-                  size="17px"
-                  />
-                </div>
-              </div>
 
-              <div className="border-t border-light-font justify-center items-center w-full"></div>
-              
+                <div className="">
+                  <ShoppingCartButton size="17px" />
+                </div>
+              </div> */}
+
+              {/* <div className="border-t border-light-font justify-center items-center w-full"></div> */}
+
               {/* Quick links that redirect to pages */}
               <div className="flex flex-row w-full justify-between items-center p-2 text-super-small">
                 <SearchQuickLinks />
@@ -112,8 +108,55 @@ const MegaSearch: React.FC<MegaSearchProps> = ({
 
               <div className="grid grid-cols-1 w-2/3 justify-start items-start mt-2 h-full">
                 {/* Search Bar */}
-                <div className="h-full">
-                  <select
+                <div className="h-full w-full">
+                  <div className="flex flex-row w-full justify-between items-center">
+                    <button
+                      className={cn(
+                        "flex justify-start text-xs font-medium transition-colors hover:text-stone-900 hover:underline hover:cursor-pointer",
+                        searchBy === "Product"
+                          ? "text-stone bg-light-background rounded-md p-1 flex justify-center items-center text-light-font underline transition-transform animate-pulse"
+                          : "text-stone-500"
+                      )}
+                      onClick={() => setSearchBy("Product")}
+                    >
+                    Product
+                    </button>
+                    <button
+                      className={cn(
+                        "flex justify-start text-xs font-medium transition-colors hover:text-stone-900 hover:underline hover:cursor-pointer",
+                        searchBy === "Seller"
+                          ? "text-stone bg-light-background rounded-md p-1 flex justify-center items-center text-light-font underline transition-transform animate-pulse"
+                          : "text-stone-500"
+                      )}
+                      onClick={() => setSearchBy("Seller")}
+                    >
+                    Seller
+                    </button>
+                    <button
+                      className={cn(
+                        "flex justify-start text-xs font-medium transition-colors hover:text-stone-900 hover:underline hover:cursor-pointer",
+                        searchBy === "Category"
+                          ? "text-stone bg-light-background rounded-md p-1 flex justify-center items-center text-light-font underline transition-transform animate-pulse"
+                          : "text-stone-500"
+                      )}
+                      onClick={() => setSearchBy("Category")}
+                    >
+                    Category
+                    </button>
+                    <button
+                      className={cn(
+                        "flex justify-start text-xs font-medium transition-colors hover:text-stone-900 hover:underline hover:cursor-pointer",
+                        searchBy === "Designer"
+                          ? "text-stone bg-light-background rounded-md p-1 flex justify-center items-center text-light-font underline transition-transform animate-pulse"
+                          : "text-stone-500"
+                      )}
+                      onClick={() => setSearchBy("Designer")}
+                    >
+                    Designer
+                    </button>
+                  </div>
+
+                  {/* <select
                     className="flex flex-col justify-center items- text-xs p-3"
                     name="searchBy"
                     id="searchBy"
@@ -121,23 +164,37 @@ const MegaSearch: React.FC<MegaSearchProps> = ({
                     onChange={(e) => setSearchBy(e.target.value)}
                   >
                     <option value="Product">Search by: Product</option>
-                    <option value="Other">Search by: Seller, Designer or Category</option>
-                  </select>
-                  {searchBy === 'Product' ? (
-                    <SearchInputAndResultsProducts
-                      label="Search entire store..."
-                    />
-                  ) : (
-                    <SearchInputAndResultsSellers
+                    <option value="Seller">Search by: Seller</option>
+                    <option value="Category">Search by: Category</option>
+                    <option value="Designer">Search by: Designer</option>
+                  </select> */}
+                  {searchBy === "Product" ? (
+                    <SearchInputAndResultsProducts label="Search entire store..." />
+                  ) : searchBy === "Seller" ? (
+                    <SearchInputAndResultsData
                       label="Search by Seller..."
+                      route="/sellers"
+                      flag="seller"
+                      name="SELLERS"
                     />
-                  )}
+                  ) : searchBy === "Category" ? (
+                    <SearchInputAndResultsData
+                      label="Search by Category..."
+                      route="/categories"
+                      flag="category"
+                      name="CATEGORIES"
+                    />
+                  ) : searchBy === "Designer" ? (
+                    <SearchInputAndResultsData
+                      label="Search by Designer..."
+                      route="/designers"
+                      flag="designer"
+                      name="DESIGNERS"
+                    />
+                  ) : null}
                 </div>
               </div>
             </div>
-
-
-            
           </Dialog.Panel>
         </div>
       </Dialog>
@@ -146,5 +203,3 @@ const MegaSearch: React.FC<MegaSearchProps> = ({
 };
 
 export default MegaSearch;
-
-
